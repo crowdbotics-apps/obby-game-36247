10de083fa1fc2df65144464462c1ffa78fd08c49
Crowdbotics <team@crowdbotics.com>
2022-07-17 20:13:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "obby_game_36247",
+  url: "https://obby_game_36247.botics.co",
+  api: "https://obby_game_36247.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
